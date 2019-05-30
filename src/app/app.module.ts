8feb856ee7a731e8ffd47ec0b28ac51217f086d8
Sydney Jeffries <sydney.jeffriess@gmail.com>
2019-05-30 18:13:40 -0500

    Tours component
    added a new component and
    imported the fonts needed for the project.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './component/footer/footer.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { ToursComponent } from './component/tours/tours.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    ToursComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+  
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
